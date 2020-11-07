4a804caffdf646e272a434b37a4d8fbf7d99f12f
Game111fff <964851688@qq.com>
2020-11-07 21:10:46 +0800

    更新一

---
+++ HelloGit/src/test/java/HelloGitTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2020-11-07
+ * Time: 20:44
+ */
+
+public class HelloGitTest
+{
+    @Test
+    public void test(){
+        System.out.println("更新一");
+    }
+}
